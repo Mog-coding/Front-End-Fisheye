11b2701002919ef75224f24fa9224983f406d56c
Morgan <morgand@tutamail.com>
2022-02-04 14:44:10 +0100

    Trie image / video Media fonctionnel

--- scripts/pages/photographerPage.js
+++ scripts/pages/photographerPage.js
@@ -5,14 +5,16 @@ import Image from "../model/Image.js"
 import Video from "../model/Video.js"
 
 let inverse = 0;
-/*RECUP DES DONNEES */
+/*RECUPERATION DES DONNEES */
 fetch('data/photographers.json')    //promise1 résolue: serveur répond
     .then(function (response) {     //promise2 résolue: data chargée
         return response.json();     //data json vers objet
     })
     .then(function ({ media, photographers }) { //resolve p2 donne data formatée 
 
-        //partie PHOTOGRAPHERS
+        /**
+         * partie PHOTOGRAPHERS BANNIERE
+         */
         const url_object = window.location;
         let objetParam = new URL(url_object);
         const identifiant = Number(objetParam.searchParams.get('id'));
@@ -20,38 +22,80 @@ fetch('data/photographers.json')    //promise1 résolue: serveur répond
             return objet.id === identifiant
         });                        //[{name:"Keel", id: 12}]
         dataMain = dataMain[0];    //{name:"Keel", id: 12}
-        
+
         //template photographers
         const VueMain = new PhotographerFactory(dataMain);
         document.querySelector("#main").appendChild(VueMain.createPhotographerBanner());
 
-        //partie MEDIA                 media = [{key1:'t', key2:'t'}, {...}];
-        let dataMedia = media.filter(function (objet) {   //trie medias
+
+        /**
+         * partie MEDIA
+         */
+        //function crée Vue Image ou Video en fonction de la data
+        function createImageVideoCard(dataMedia) {
+            // --> sortie tableau d'instances de class image ou video extension Media
+            //wrappé dans la classe MediaFactory contenant méthode builder image/videoCard
+            let dataMediaInstance = dataMedia.map(function (el) {
+                return new MediaFactory(el)
+            })
+            //Création Vue template Media via méthode builder class MediaFactory 
+            dataMediaInstance.forEach(function (el) {
+                if (el.media instanceof Image) {
+                    document.querySelector(".containerPhotos").appendChild(el.createImageCard());
+                } else if (el.media instanceof Video) {
+                    document.querySelector(".containerPhotos").appendChild(el.createVideoCard());
+                }
+            }
+            );
+        }
+
+        //extrait un objet d'un tableau d'objets en fonction de son identifiant  
+        let dataMedia = media.filter(function (objet) {
             return objet.photographerId === identifiant
-        });        //media = [{key1:'t', key2:'t'}, {...}];
-        dataMedia = dataMedia.map(function (el) {//instance Image/Video
-            return new MediaFactory(el)  //{_key1:'t', _key2:'t'} +héritage 
-        })
-        .sort(function(a, b){
-            return a.likes - b.likes;
         });
 
-        //Bouton dropdown
-        document.querySelector("#dropdown").addEventListener("change", function(){
-            console.log(document.querySelector("#dropdown").value);
-        })
-        console.log(dataMedia);
+        createImageVideoCard(dataMedia);
 
-        //template Media
-        dataMedia.forEach(function (el) {
-            //console.log(el);
-            if (el.media instanceof Image) {
-                document.querySelector(".containerPhotos").appendChild(el.createImageCard());
-            } else if (el.media instanceof Video) {
-                document.querySelector(".containerPhotos").appendChild(el.createVideoCard());
+        // Bouton dropdown 
+        document.querySelector("#dropdown").addEventListener("change", function () {
+            const menuSelect = document.querySelector("#dropdown").value;
+            console.log(menuSelect);
+            if (menuSelect === 'Titre') {
+                dataMedia.sort(function (a, b) {
+                    if (a.title > b.title) return 1;
+                    if (a.title < b.title) return -1;
+                    return 0;
+                })
+                const nodeMedia = document.querySelector(".containerPhotos");
+                while (nodeMedia.firstChild) {
+                    nodeMedia.removeChild(nodeMedia.lastChild);
+                }
+                createImageVideoCard(dataMedia);
+                //console.log(dataMedia);
             }
-        }
-        );
+            else if (menuSelect === 'Popularité') {
+                dataMedia.sort(function (a, b) {
+                    return b.likes - a.likes
+                })
+                const nodeMedia = document.querySelector(".containerPhotos");
+                while (nodeMedia.firstChild) {
+                    nodeMedia.removeChild(nodeMedia.lastChild);
+                }
+                createImageVideoCard(dataMedia);
+            }
+            else if (menuSelect === 'Date') {
+                dataMedia.sort(function (a, b) {
+                    if (a.date > b.date)  return 1;		
+                    if (a.date < b.date)  return -1;		
+                    return 0;
+                })
+                const nodeMedia = document.querySelector(".containerPhotos");
+                while (nodeMedia.firstChild) {
+                    nodeMedia.removeChild(nodeMedia.lastChild);
+                }
+                createImageVideoCard(dataMedia);
+            }
+        })
 
         // Like coeur
         document.querySelectorAll(".heart").forEach(function (el) {
@@ -71,8 +115,4 @@ fetch('data/photographers.json')    //promise1 résolue: serveur répond
                 }
             })
         });
-
-
     })
-
-
--- test.js
+++ test.js
@@ -1,14 +1,5 @@
-class MoviesFactory {
-    constructor(data, type) {
-        // Si le type correspond à l'ancienne API, alors retourne-moi l'ancien formatage
-        if (type === 'oldApi') {
-            return new OldMovie(data)
-        // Sinon retourne-moi le nouveau formatage
-        } else if (type === 'newApi') {
-            return new Movie(data)
-        // Une bonne pratique est de déclencher une erreur si le format n'est pas reconnu
-        } else {
-            throw 'Unknown type format'
-        }
-    }
- }
+const array1 = [{key:1}, {key:2}, {key:3}];
+array1.reverse();
+console.log(array1);
+array1.reverse();
+console.log(array1);
